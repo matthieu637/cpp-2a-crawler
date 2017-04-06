fcd7e15f94f85064a0611d4bfc7f9b24cf4dc752
David Desobry <david.desobry@orange.fr>
2017-04-06 21:24:01 +0200

    ajout Rudder format factory

---
+++ crawler/src/cpp2017/rudder/Rudder1.java
@@ -0,0 +1,12 @@
+package cpp2017.rudder;
+
+public class Rudder1 extends Rudder {
+
+	public Rudder1(){
+		//constructeur à ajouter
+	}
+	@Override
+	public void afficheTypeRudder() {
+		System.out.println("Rudder1");
+	}
+}
---
+++ crawler/src/cpp2017/rudder/RudderFactory.java
@@ -0,0 +1,17 @@
+package cpp2017.rudder;
+
+public class RudderFactory {
+	public static final int TYPE_RUDDER1=1;
+	
+	public Rudder getRudder(int typeRudder){
+		Rudder r;
+		switch (typeRudder){
+		case TYPE_RUDDER1:
+			r=new Rudder1();
+			break;
+		default:
+			throw new IllegalArgumentException("Type de rudder inconnu");
+		}
+	return r;
+	}
+}
