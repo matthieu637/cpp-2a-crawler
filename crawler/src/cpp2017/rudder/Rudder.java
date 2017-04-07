8f7c5bd676f5befbba0a55bac76afc1e52e458a9
david540 <david.desobry@orange.fr>
2017-04-07 13:24:56 +0200

    Update Rudder.java

--- crawler/src/cpp2017/rudder/Rudder.java
+++ crawler/src/cpp2017/rudder/Rudder.java
@@ -1,6 +1,10 @@
 package cpp2017.rudder;
 
+/**
+ * @author David
+ * Tout les "gouvernails" hériteront de cette classe mère abstraite
+ * Celà permettra le polymorphisme
+ */
 public abstract class Rudder {
-	public abstract void afficheTypeRudder();
+	public abstract String toString();
 }
-
