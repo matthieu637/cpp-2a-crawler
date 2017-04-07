6d889c427635c83ac7bd02013aab5b0148d1f693
MatthieuDEVALLE <matthieu.devalle@live.fr>
2017-04-07 15:05:19 +0200

    Récupération des liens d'une page

--- crawler/src/cpp2017/main.java
+++ crawler/src/cpp2017/main.java
@@ -1,11 +1,20 @@
 package cpp2017;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import cpp2017.parser.Parser;
 import cpp2017.rudder.Rudder1;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Rudder1 testRudder=new Rudder1();
-		testRudder.afficheTypeRudder();
+		System.out.println(testRudder.toString());
+		
+		Parser testParser=new Parser("http://arche.univ-lorraine.fr");
+		System.out.println(testParser.getLinks());
 	}
 
 }
