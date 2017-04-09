9deb59d05482f056257931c8e081a1b8c8e22f6e
david540 <david.desobry@orange.fr>
2017-04-09 15:01:14 +0200

    Ajout commentaires

--- crawler/src/cpp2017/test/ParserTest.java
+++ crawler/src/cpp2017/test/ParserTest.java
@@ -8,20 +8,42 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * @author david
+ * Tests unitaires du Parser
+ * 
+ */
+
 public class ParserTest {
 
+	/**
+	 * @throws Exception
+	 * Est appelé avant tous les tests
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
+	/**
+	 * @throws Exception
+	 * Est appelé après tous les tests
+	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 * @throws Exception
+	 * Est appelé avant chaque test
+	 */
 	@Before
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * @throws Exception
+	 * Est appelé après chaque test
+	 */
 	@After
 	public void tearDown() throws Exception {
 	}
