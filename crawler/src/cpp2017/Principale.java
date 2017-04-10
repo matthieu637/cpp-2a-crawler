172c9755811f9ed9b3b3f89b8009853fc269de9e
MatthieuDEVALLE <matthieu.devalle@live.fr>
2017-04-10 10:53:33 +0200

    Compléments pour le Parser : liens, h1, titre, strong  + Doc

--- crawler/src/cpp2017/Principale.java
+++ crawler/src/cpp2017/Principale.java
@@ -3,11 +3,12 @@ package cpp2017;
 import java.io.IOException;
 import cpp2017.parser.Parser;
 
+
 public class Principale {
 
 	public static void main(String[] args) throws IOException {
-		Parser testParser = new Parser("http://arche.univ-lorraine.fr");
-		System.out.println(testParser.getLinks());
+		Parser testParser = new Parser("http://arche.univ-lorraine.fr/");
+		System.out.println(testParser.getInfos());
 
 	}
 
--- crawler/src/cpp2017/parser/Parser.java
+++ crawler/src/cpp2017/parser/Parser.java
@@ -2,14 +2,18 @@ package cpp2017.parser;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Vector;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Parser {
-	/** @author MatthieuDEVALLE
-	 * Classe permettant de parser une page web à partir d'un attribut url 
+	/**
+	 * @author MatthieuDEVALLE 
+	 * Classe permettant de parser une page web à partir
+	 * d'un attribut url
 	 */
 	String url;
 
@@ -22,23 +26,40 @@ public class Parser {
 	// return new String(doc.title());
 	// }
 	/**
-	 * @return 
+	 * @return
 	 * @throws IOException
-	 * On retourne une HashMap contenant les objets java.
-	 * Pour récupérer les liens, on se connecte avec jsoup au site et on récupère
-	 * les balises <a href="">, c'est à dire les balises HTML qui représentent les liens.
+	 *             On retourne une HashMap contenant les objets java. Pour
+	 *             récupérer les liens, on se connecte avec jsoup au site et on
+	 *             récupère les balises <a href="">, c'est à dire les balises
+	 *             HTML qui représentent les liens. Idem pour les autres catégories
 	 */
-	public HashMap<Integer, String> getLinks() throws IOException {
-		HashMap<Integer, String> LinksList = new HashMap<Integer, String>();
-		int i = 0;
+	public HashMap<String, Vector<String>> getInfos() throws IOException {
+		HashMap<String, Vector<String>> contentParse = new HashMap<String, Vector<String>>();
+		Vector<String> linksList = new Vector<String>();
+		Vector<String> strongList = new Vector<String>();
+		Vector<String> H1List = new Vector<String>();
+		Vector<String> titreList = new Vector<String>(1);
 		Document page = Jsoup.connect(this.url).get();
+		String titre = page.title();
+		Elements titreH1s = page.select("h1");
+		Elements strongs = page.select("strong");
 		Elements links = page.select("a[href]");
-		for (Element link : links) {
-			LinksList.put(i, link.attr("abs:href"));
-			i++;
-		}
 
-		return LinksList;
+		titreList.add(titre);
+		for (Element titreH1 : titreH1s) {
+			H1List.add(titreH1.attr("h1"));
+		}
+		for (Element link : links) {
+			linksList.add(link.attr("abs:href"));
+		}
+		for (Element strong : strongs) {
+			strongList.add(strong.attr("strong"));
+		}
+		contentParse.put("titre", titreList);
+		contentParse.put("h1", H1List);
+		contentParse.put("strong", strongList);
+		contentParse.put("liens", linksList);
+		return contentParse;
 
 	}
 
