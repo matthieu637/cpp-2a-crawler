1247244b112230545dc6c7a7f6ffddf7b66b9593
David Desobry <david.desobry@orange.fr>
2017-04-12 15:12:09 +0200

    Fabrique de Parser + premier main() #33

--- crawler/src/cpp2017/Principale.java
+++ crawler/src/cpp2017/Principale.java
@@ -1,7 +1,11 @@
 package cpp2017;
 
 import java.io.IOException;
+import java.util.Arrays;
+
+import cpp2017.parser.LinkQueue;
 import cpp2017.parser.Parser;
+import cpp2017.parser.ParserFactory;
 import cpp2017.rudder.Rudder;
 import cpp2017.rudder.RudderFactory;
 
@@ -10,10 +14,18 @@ public class Principale {
 
 	public static void main(String[] args) throws IOException {
 		//Creation d'un Rudder Naif, limite de nombres de liens à parser déterminée dans CrawlerConfig.java
-		Rudder testRudder1 = RudderFactory.getInstance().getRudder(RudderFactory.TYPE_NAIVE_RUDDER);
-		System.out.println(testRudder1);
-		Parser testParser = new Parser("http://arche.univ-lorraine.fr/");
-		System.out.println(testParser.getInfos());
+		Rudder testNaiveRudder = RudderFactory.getInstance().getRudder(RudderFactory.TYPE_NAIVE_RUDDER);
+		System.out.println(testNaiveRudder);
+		Parser testJsoupParser = ParserFactory.getInstance().getParser(ParserFactory.TYPE_JSOUP_PARSER);
+		//Parser testParser = new Parser("http://arche.univ-lorraine.fr/");
+		//System.out.println(testParser.getInfos());
+		testJsoupParser.registerRudder(testNaiveRudder);
+		LinkQueue.getInstance().registerParser(testJsoupParser);
+		
+		testNaiveRudder.addLink(Arrays.asList("http://arche.univ-lorraine.fr/"));
+		
+		testJsoupParser.start();
+		
 	}
 
 }
