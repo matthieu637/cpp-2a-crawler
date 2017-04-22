97f89a9bd209b68278557392fadf3fa876c233b5
David Desobry <david.desobry@orange.fr>
2017-04-22 14:08:29 +0200

    Remplacement synchronized par ReadWriteLock

--- crawler/src/cpp2017/parser/LinkQueue.java
+++ crawler/src/cpp2017/parser/LinkQueue.java
@@ -6,6 +6,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author David
@@ -18,6 +21,9 @@ public final class LinkQueue {
 
 	private static LinkQueue instance;
 	private List<Parser> lParser;
+	private final ReadWriteLock mutex_ordre = new ReentrantReadWriteLock();
+	private final Lock mutex_ordre_read = mutex_ordre.readLock();
+	private final Lock mutex_ordre_write = mutex_ordre.writeLock();
 	/**
 	 * Ensemble des liens déjà présenté à la queue. HashSet pour test
 	 * d'appartenance en O(1).
@@ -57,7 +63,9 @@ public final class LinkQueue {
 	}
 
 	public void registerParser(Parser p) {
+		mutex_ordre_write.lock();
 		lParser.add(p);
+		mutex_ordre_write.unlock();
 	}
 
 	/**
@@ -66,15 +74,20 @@ public final class LinkQueue {
 	 * 
 	 * @param lien
 	 */
-	public synchronized void addPriorityLink(PriorityLink lien) {
+	public void addPriorityLink(PriorityLink lien) {
 		// verrou pour l'écriture
+		mutex_ordre_write.lock();
 		queueLien.add(lien);
 		histoLink.add(lien.getUrl());
+		mutex_ordre_write.unlock();
 		this.notifyAll();
 	}
 
-	public synchronized int size() {
-		return queueLien.size();
+	public int size() {
+		mutex_ordre_read.lock();
+		int l = queueLien.size();
+		mutex_ordre_read.unlock();
+		return l;
 	}
 
 	/**
@@ -83,40 +96,58 @@ public final class LinkQueue {
 	 * 
 	 * @return PriorityLink
 	 */
-	public synchronized PriorityLink getLink() throws InterruptedException {
-		while (queueLien.isEmpty())
+	public PriorityLink getLink() throws InterruptedException {
+		mutex_ordre_write.lock();
+		while (queueLien.isEmpty()){
+			mutex_ordre_write.unlock();
 			wait();
-
+			mutex_ordre_write.lock();
+		}
 		PriorityLink linkToParse = queueLien.pollLast();
+		mutex_ordre_write.unlock();
 		return linkToParse; // retourne le lien le mieux classé ou null si
 							// queueLien est une liste vide
 	}
 
-	public synchronized boolean alreadyParsed(String newLink) {
+	public boolean alreadyParsed(String newLink) {
+		mutex_ordre_read.lock();
 		for (String link : histoLink) {
-			if (link.equals(newLink))
+			if (link.equals(newLink)){
+				mutex_ordre_read.unlock();
 				return true;
+			}
 		}
+		mutex_ordre_read.unlock();
 		return false;
 	}
 
-	public synchronized int getPriorityOfLastElem() {
-		if (queueLien.isEmpty())
+	public int getPriorityOfLastElem() {
+
+		mutex_ordre_read.lock();
+		if (queueLien.isEmpty()){
+			mutex_ordre_read.unlock();
 			return 0;
-		return this.queueLien.first().getPriority();
+		}
+		int p= this.queueLien.first().getPriority();
+		mutex_ordre_read.unlock();
+		return p;
 	}
 
-	public synchronized void clearAll() {
+	public void clearAll() {
+		mutex_ordre_write.lock();
 		queueLien.clear();
 		histoLink.clear();
+		mutex_ordre_write.unlock();
 	}
 
-	public synchronized void removeAlreadyParsed(List<String> links) {
+	public void removeAlreadyParsed(List<String> links) {
+		mutex_ordre_write.lock();
 		for (Iterator<String> it = links.iterator(); it.hasNext();) {
 			String l = it.next();
 			if (histoLink.contains(l))
 				it.remove();
 		}
+		mutex_ordre_write.unlock();
 	}
 
 }
